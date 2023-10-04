f02aaa7f66272e3d5244e821d56dc5c7b0ed9b67
bayuuat <bayuuat@gmail.com>
2023-10-04 22:01:05 +0700

    feat: crud departments

--- src/pages/Departments/components/ModalCreate.jsx
+++ src/pages/Departments/components/ModalCreate.jsx
@@ -3,16 +3,63 @@ import React, { useEffect, useState } from 'react';
 import Modal from '@/components/modal';
 import InputField from '@/components/fields/InputField';
 import { BiSolidBox } from 'react-icons/bi';
+import customAxios from '@/utils/customAxios';
 
-const ModalCreate = ({ open, onClose, data }) => {
+const ModalCreate = ({ open, onClose, data, refetch }) => {
   const [form, setForm] = useState({ departmentID: '', departmentName: '' });
+  const [word, setWord] = useState('');
+
+  const postDepartment = async (e) => {
+    e.preventDefault();
+    const submitForm = { departmentName: form.departmentName };
+    try {
+      const response = await customAxios.post('/departments/create', submitForm);
+
+      if (response.status === 200) {
+        refetch();
+        onClose();
+        resetInput();
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        alert(error.response.data);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  const updateDepartment = async (e) => {
+    e.preventDefault();
+    try {
+      const submitForm = { departmentName: form.departmentName, departmentID: form.departmentID };
+      const response = await customAxios.put(`/departments/update/${form.departmentID}`, submitForm);
+
+      if (response.status === 200) {
+        refetch();
+        onClose();
+        resetInput();
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        alert(error.response.data);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  const resetInput = () => {
+    setForm({ departmentID: 'New Department', departmentName: '' });
+  };
 
   useEffect(() => {
-    console.log(data);
     if (data === 'new') {
-      setForm({ departmentID: 'New Department', departmentName: '' });
+      resetInput();
+      setWord('Create');
     } else if (data) {
       setForm(data);
+      setWord('Update');
     }
   }, [data]);
 
@@ -21,49 +68,64 @@ const ModalCreate = ({ open, onClose, data }) => {
     setForm({ ...form, [name]: value });
   };
 
+  const handleOnSubmit = (e) => {
+    data === 'new' ? postDepartment(e) : updateDepartment(e);
+  };
+
   return (
     <Modal open={open} onClose={onClose}>
-      <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div className="flex items-center mb-3">
-          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
-            <BiSolidBox className="text-orange-600 text-xl" />
+      <form onSubmit={handleOnSubmit}>
+        <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="flex items-center mb-3">
+            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
+              <BiSolidBox className="text-orange-600 text-xl" />
+            </div>
+            <h3 className="ml-3 text-lg leading-6 font-medium text-gray-900 dark:text-white">{word} New Department</h3>
           </div>
-          <h3 className="ml-3 text-lg leading-6 font-medium text-gray-900 dark:text-white">Create New Department</h3>
+          <div className="mt-4">
+            <InputField
+              extra="mb-4"
+              label="ID"
+              placeholder="123456"
+              id="departmentID"
+              name="departmentID"
+              type="text"
+              value={form.departmentID}
+              onChange={handleFormChange}
+              disabled
+            />
+            <InputField
+              extra="mb-4"
+              label="Department Name"
+              placeholder="Human Resource"
+              id="departmentName"
+              name="departmentName"
+              type="text"
+              value={form.departmentName}
+              onChange={handleFormChange}
+            />
+          </div>
         </div>
-        <div className="mt-4">
-          <InputField extra="mb-4" label="ID" placeholder="123456" id="departmentID" type="text" value={form.departmentID} disabled />
-          <InputField
-            extra="mb-4"
-            label="Department Name"
-            placeholder="Human Resource"
-            id="departmentName"
-            type="text"
-            name="departmentName"
-            value={form.departmentName}
-            onChange={handleFormChange}
-          />
+        <div className="bg-gray-50 dark:bg-navy-900 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+            <button
+              type="submit"
+              className="inline-flex justify-center w-full rounded-md px-4 py-2 bg-orange-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+            >
+              {word}
+            </button>
+          </span>
+          <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+            <button
+              onClick={onClose}
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-transparent text-base leading-6 font-medium text-gray-700 dark:text-white dark:border-navy-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+            >
+              Cancel
+            </button>
+          </span>
         </div>
-      </div>
-      <div className="bg-gray-50 dark:bg-navy-900 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-          <button
-            onClick={() => {}}
-            type="button"
-            className="inline-flex justify-center w-full rounded-md px-4 py-2 bg-orange-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-          >
-            Create
-          </button>
-        </span>
-        <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-          <button
-            onClick={onClose}
-            type="button"
-            className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-transparent text-base leading-6 font-medium text-gray-700 dark:text-white dark:border-navy-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-          >
-            Cancel
-          </button>
-        </span>
-      </div>
+      </form>
     </Modal>
   );
 };
--- src/pages/Departments/index.jsx
+++ src/pages/Departments/index.jsx
@@ -1,8 +1,9 @@
-import Modal from '@/components/modal';
+import { useEffect, useState } from 'react';
+
 import DevTableTemplate from '@/components/table/DevelopmentTable';
 import ModalCreate from '@/pages/Departments/components/ModalCreate';
 import customAxios from '@/utils/customAxios';
-import { useEffect, useState } from 'react';
+import ModalDelete from '@/components/modal/ModalDelete';
 
 const columnData = [
   {
@@ -30,14 +31,32 @@ const columnData = [
 const Department = () => {
   const [data, setData] = useState([]);
   const [openModal, setOpenModal] = useState(false);
+  const [openModalDelete, setOpenModalDelete] = useState(false);
   const [selectedData, setSelectedData] = useState({});
 
   const getTicketTagList = async () => {
     try {
-      const response = await customAxios.get('/departments/list');
-
+      const response = await customAxios.get('/departments/view');
       if (response.status === 200) {
         setData(response.data);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        alert(error.response.data);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  const deleteDepartment = async () => {
+    try {
+      const response = await customAxios.delete(`/departments/delete/${selectedData.departmentID}`);
+
+      if (response.status === 204) {
+        alert('Success Deleting Data');
+        getTicketTagList();
+        handleCloseModalDelete();
       }
     } catch (error) {
       if (error.response && error.response.data) {
@@ -54,7 +73,16 @@ const Department = () => {
 
   const handleOpenModal = (data) => {
     setOpenModal(true);
-    setSelectedData(data)
+    setSelectedData(data);
+  };
+
+  const handleCloseModalDelete = () => {
+    setOpenModalDelete(false);
+  };
+
+  const handleOpenModalDelete = (data) => {
+    setOpenModalDelete(true);
+    setSelectedData(data);
   };
 
   useEffect(() => {
@@ -63,11 +91,18 @@ const Department = () => {
 
   return (
     <>
-      <p>{openModal}</p>
       <div className="mt-5">
-        <DevTableTemplate title={'Deparments Table'} columnsData={columnData} tableData={data} buttonText={"Create New Department"} onClickEdit={handleOpenModal} />
+        <DevTableTemplate
+          title={'Deparments Table'}
+          columnsData={columnData}
+          tableData={data}
+          buttonText={'Create New Department'}
+          onClickEdit={handleOpenModal}
+          onClickDelete={handleOpenModalDelete}
+        />
       </div>
-      <ModalCreate open={openModal} onClose={handleCloseModal} data={selectedData}/>
+      <ModalCreate open={openModal} onClose={handleCloseModal} data={selectedData} refetch={getTicketTagList} />
+      <ModalDelete open={openModalDelete} onClose={handleCloseModalDelete} selectedData={selectedData?.departmentName} onDelete={deleteDepartment} />
     </>
   );
 };
